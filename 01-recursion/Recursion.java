497d3f7d0841f41d4dd01804a2c816f44b51b774
John Doe <rhossain30@stuy.edu>
2022-02-04 00:40:30 -0500

    no repeat func

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -1,5 +1,8 @@
 public class Recursion{
     public static final char[] a2e = {'a','b','c','d','e'};
+    public static final char[] a2c = {'a','b','c'};
+
+
     public static void printAllWords(int length){
         printAllWords(length,"");
     }
@@ -13,27 +16,28 @@ public class Recursion{
             }
         }
     }
-        /*Print all words that are made of the characters in the array of letters.
-    *There may not be consecutive equal letters, so:
-    *aax is not allowed, but axa is allowed.
-    *@param length : the length of the words that are to be printed
-    *@param letters: the letters you should be using,
-    *@precondition: letters contains at least 2 characters, and has no duplicates.
-    */
+
     public static void printNoDoubleLetterWords(int length,char[] letters){
         printNoDoubleLetterWords(length,"",letters);
     }
 
-    /*Print all words that are made of the characters in letters. There may not be consecutive equal letters,
-    *aax is not allowed, but axa is allowed.
-    *@param length : either how many more letters need to be
-    *@param word   : the partial word so far.
-    *@param letters: the letters you should be using
-    */
     public static void printNoDoubleLetterWords(int length,String word,char[]letters){
-        //WRITE THIS METHOD
+        if (length==0){
+            boolean t = true;
+            for(int i = 0;i<word.length()-1;i++){
+                if (word.charAt(i) == word.charAt(i+1)){
+                    t = false;
+                    break;
+                }
+            }
+            if (t) System.out.println(word);
+        }else{
+            for(int i = 0;i<letters.length;i++){
+                printNoDoubleLetterWords(length-1, word + letters[i], letters);
+            }
+        }
     }
     public static void main(String[] args){
-        printAllWords(3);
+        printNoDoubleLetterWords(3, "", a2c);
     }
 }
