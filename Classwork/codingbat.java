0990b03ffc46eb896e9dd1f6b2baac3a3fb7efa2
baka <rhossain30@stuy.edu>
2022-02-09 14:39:14 -0500

    RecursionClasswork  groupsum

--- Classwork/codingbat.java
+++ Classwork/codingbat.java
@@ -1,5 +1,13 @@
 public class codingbat{
+	public static boolean groupSum(int start, int[] nums, int target) {
+		if (start>=nums.length) return (target == 0);
+		return (groupSum(start+1, nums, target) || groupSum(start+1, nums, target-nums[start]));
+	  }	  
 	public static void main(String[] args){
-
+		int[] num = {2,4,8};
+		System.out.println(groupSum(0,num,10));
+		System.out.println(groupSum(0,num,9));
+		System.out.println(groupSum(0,num,14));
+		
 	}
 }
