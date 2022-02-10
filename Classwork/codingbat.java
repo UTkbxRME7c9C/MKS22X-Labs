4b37c774659b2c1cceb33657d2e2bacb1872fec2
baka <rhossain30@stuy.edu>
2022-02-10 11:29:02 -0500

    RecursionClasswork  groupsum5

--- Classwork/codingbat.java
+++ Classwork/codingbat.java
@@ -14,18 +14,27 @@ public class codingbat{
 	}
 	public static boolean groupSum6(int start,int[] nums, int target){
 		if(start>= nums.length) return target==0;
-		if (nums[start] == 6) return (groupSum6(start+1, nums, target-6) || groupSum6(start+1, nums, (target-6)-nums[start]));
+		if (nums[start] == 6) return (groupSum6(start+1, nums, target-6));
 		return (groupSum6(start+1, nums, target) || groupSum6(start+1, nums, target-nums[start]));
+	}
+	public static boolean groupSum5(int start, int[] nums, int target){
+		if(start>= nums.length) return target==0;
+		if (nums[start]%5==0) return groupSum5(start+1, nums, (target)-nums[start]);
+		if ((start>0 && nums[start] == 1) && nums[start-1]%5 == 0) return groupSum5(start+1, nums, target);
+		return (groupSum5(start+1, nums, target) || groupSum5(start+1, nums, target-nums[start]));
 	}
 
 	public static void main(String[] args){
-		// int[] num = {2,4,8};
 		// System.out.println(groupSum(0,num,10));
 		// System.out.println(groupSum(0,num,9));
 		// System.out.println(groupSum(0,num,14));
-		int[] num2 = {2,3};
-		int[] num3 = {5,2,3};
-		System.out.println(splitArray(num2));
-		System.out.println(splitArray(num3));		
+		int[] num3 = {2,5,10,4};
+		//System.out.println(splitArray(num2));
+		//System.out.println(splitArray(num3));	
+		// System.out.println(groupSum6(0, num3, 8));	
+		// System.out.println(groupSum6(0, num3, 9));	
+		System.out.println(groupSum5(0, num3, 19));	
+		System.out.println(groupSum5(0, num3, 12));
+
 	}
 }
