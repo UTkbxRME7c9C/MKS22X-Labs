1ca8c32d57bb515f7717022d21f0af860e981df7
baka <rhossain30@stuy.edu>
2022-02-10 13:55:40 -0500

    RecursionClasswork  groupsumclump

--- Classwork/codingbat.java
+++ Classwork/codingbat.java
@@ -27,11 +27,29 @@ public class codingbat{
 		if(start>= nums.length) return target==0;
 		return (groupNoAdj(start+1, nums, target) || groupNoAdj(start+2, nums, target-nums[start]));
 	}
-
+	public static boolean groupSumClump(int start, int[] nums, int target) {
+		int rep = 0;
+		int repi = 1;
+		for(int i = 1; i < nums.length;i++){
+			if (nums[i]==nums[i-1]){
+				rep = nums[i];
+				repi++;
+			}
+		}
+		if (repi==1) return groupSum(start, nums, target);
+		return groupSumClump(start, nums, target, rep, repi);
+	}
+	public static boolean groupSumClump(int start, int[] nums, int target, int rep, int repi) {
+		if(start>= nums.length) return target==0;
+		if (nums[start]==rep) return (groupSumClump(start+repi, nums, target-(rep*repi),rep,repi)|| groupSumClump(start+repi, nums, target, rep, repi));
+		return (groupSumClump(start+1, nums, target, rep, repi)|| groupSumClump(start+1, nums, target-nums[start], rep, repi));
+	}
 	public static void main(String[] args){
-		int[] num3 = {2,5,10,4};	
-		System.out.println(groupNoAdj(0, num3, 14));	
-		System.out.println(groupNoAdj(0, num3, 12));
+		int[] num = {1,2,4,8,1};	
+		int[] num2 = {2,4,4,8};
+		System.out.println(groupSumClump(0, num, 14));
+		System.out.println(groupSumClump(0, num2, 14));
+
 
 	}
 }
