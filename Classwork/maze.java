eaada86a38f4c9c9a63fb6c364b92d54938a5117
balls <rhossain30@stuy.edu>
2022-02-22 00:19:02 -0500

    nqueens 2d array maker

--- Classwork/maze.java
+++ Classwork/maze.java
@@ -1,23 +1,38 @@
 import java.io.File;
+import java.util.Arrays;
 import java.io.FileNotFoundException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Scanner;
 public class maze{
-    public static int printline(String filename) throws FileNotFoundException{
+
+    public static char[][] printline(String filename) throws FileNotFoundException{
         File fil = new File(filename);
         Scanner input = new Scanner(fil);
-        String c = "";
-        int tol = 0;
-        while(input.hasNextLine());
-            c+=input.nextInt();
-            tol++;
+        String tmp = "";
+        while(input.hasNextLine()){
+            tmp+=input.nextLine() + "\n";
+        }
         input.close();
-        return tol;
+        String[] temp = tmp.split("\n");
+        char[][] lines = new char[temp.length][];
+        for(int i = 0;i<lines.length;i++){
+            lines[i] = temp[i].toCharArray();
+        }
+        return lines;
+    }
+
+    public static String toStr(char[][] board){
+        String ans = "";
+        for(int i = 0; i < board.length;i++){
+            for(int j = 0; j < board[0].length;j++){
+                ans+=board[i][j];
+            }
+            ans+="\n";
+        }
+        return ans;
     }
     public static void main(String[] args){
         try {
-            System.out.println(printline("maze"));
+            System.out.println(toStr(printline("mze")));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
