95b793d595cbed4dfe3d246c5de8d593b29c4acb
baka <rhossain30@stuy.edu>
2022-03-02 14:05:35 -0500

    finish maze

--- 03-maze/Driver.java
+++ 03-maze/Driver.java
@@ -5,15 +5,14 @@ public class Driver{
     public static void main(String[]args){
 
       //files do not require an extension like .txt or .dat
-      String filename = "mze";
+      String filename = "maze5";
 
       try{
         Maze f;
         f = new Maze(filename);
-        // f.setAnimate(true);
-        // System.out.println(f.solve()+" steps");
-        System.out.println(f);
-        
+        f.setAnimate(true);
+        System.out.println(f.solve()+" steps");
+        System.out.println(f);        
       }catch(FileNotFoundException e){
         System.out.println("Invalid filename: "+filename);
       }
--- 03-maze/Maze.java
+++ 03-maze/Maze.java
@@ -5,21 +5,6 @@ public class Maze{
     private boolean animate;//false by default
     private int startRow,startCol;
 
-    /*Constructor loads a maze text file, and sets animate to false by default.
-    When the file is not found then:
-    throw a FileNotFoundException
-
-    You may assume the file contains a rectangular ascii maze, made with the following 4 characters:
-    '#' - Walls - locations that cannot be moved onto
-    ' ' - Empty Space - locations that can be moved onto
-    'E' - the location of the goal if any (0 or more per file)
-    'S' - the location of the start(exactly 1 per file)
-
-    You may also assume the maze has a border of '#' around the edges.
-    So you don't have to check for out of bounds!
-    Some text editors always include a newline at the end of a file, but that is not always present.
-    Make sure your file reading is able to handle this.
-    */
     public Maze(String filename) throws FileNotFoundException{
         animate = false;
         File fil = new File(filename);
@@ -58,17 +43,12 @@ public class Maze{
     }
 
     public static void clearTerminal(){
-        //erase terminal
         System.out.println("\033[2J");
     }
     public static void gotoTop(){
-        //go to top left of screen
         System.out.println("\033[1;1H");
     }
 
-    /*Return the string that represents the maze.
-    It should look like the text file with some characters replaced.
-    */
     public String toString(){
         String ans = "";
         for(int i = 0; i < maze.length;i++){
@@ -80,50 +60,50 @@ public class Maze{
         return ans;
     }
 
-    /*Wrapper Solve Function returns the helper function
-    Note the helper function has the same name, but different parameters.
-    Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
-    */
+
     public int solve(){
-        //only clear the terminal if you are running animation
         if(animate){
         clearTerminal();
         }
-        //start solving at the location of the s.
-        return solve(startRow,startCol);
+        return solve(startCol,startRow);
 
     }
 
-    /*
-    Recursive Solve function:
-
-    A solved maze has a path marked with '@' from S to E.
-
-    Returns the number of @ symbols from S to E when the maze is solved,
-    Returns -1 when the maze has no solution.
-
-    Postcondition:
-    The 'S' is replaced with '@'
-    The 'E' remain the same
-    All visited spots that were not part of the solution are changed to '.'
-    All visited spots that are part of the solution are changed to '@'
-    */
-    private int atcount = 0;
     private int solve(int row, int col){ //you can add more parameters since this is private
         //automatic animation! You are welcome.
+        animate();
+        if (maze[row][col]=='E'){
+            return 0;
+        }else if (maze[row][col]=='#' || maze[row][col] == '.' || maze[row][col]=='@'){
+            return -1;
+        }else{
+            maze[row][col] = '@';
+            animate();
+            int s = solve(row+1, col);
+            int n = solve(row-1,col);
+            int w = solve(row,col+1);
+            int e = solve(row,col-1);
+            if (s>-1){
+                return s+1;
+            } else if (n>-1){
+                return n+1;
+            } else if (w>-1){
+                return w+1;
+            } else if (e>-1){
+                return e+1;
+            }else{
+                maze[row][col] = '.';
+                animate();
+                return -1;
+            }
+        }
+    }
+
+    private void animate(){
         if(animate){
             gotoTop();
             System.out.println(this);
             wait(50);
         }
-        if (maze[row][col]=='E'){
-            return atcount;
-        }else if (maze[row][col]=='#' || maze[row][col] == '.' || maze[row][col]=='@'){
-            return -1;
-        }else{
-            
-        }
-        //COMPLETE SOLVE
-        return atcount;
     }
 }
