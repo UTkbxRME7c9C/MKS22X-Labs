6a445c371aab0b6ea10b0eccaf1718ff1bb422c1
rafaye <sony89me86player@gmail.com>
2022-03-14 23:11:12 -0400

    finished silver finally

--- 04-usaco/Silver.java
+++ 04-usaco/Silver.java
@@ -10,38 +10,61 @@ public class Silver {
             int N = input.nextInt();
             int M = input.nextInt();
             int T = input.nextInt();
-            int[][] landpaths = new int[N][M];
-            char[][] land = new char[N][M];
+            int[][][] landpaths = new int[2][N][M];
             for(int i = 0; i<N;i++){
                 String temp = input.next();
                 for(int j = 0; j<M;j++){
-                    land[i][j]=temp.charAt(j);
+                  if (temp.charAt(j) == '*'){
+                    landpaths[0][i][j] = -1;
+                    landpaths[1][i][j] = -1;
+                  }
                 }
             }
             int R1=input.nextInt()-1;
             int C1=input.nextInt()-1;
             int R2=input.nextInt()-1;
             int C2=input.nextInt()-1;
-            for (int i = 0;i<N;i++){
-                for (int j =0;j<M;j++){
-                    if (land[i][j] == '*'){
-                        landpaths[i][j]=-1;
+            landpaths[0][R1][C1] = 1;
+            landpaths[1][R1][C1] = 1;
+            input.close();
+            int first = 0;
+            int second = 1;
+            for (int time = 0;time<T;time++){
+                if (first==1){
+                  first=0;
+                  second=1;
+                }else{
+                  first=1;
+                  second=0;
+                }
+                for(int i=0;i<N;i++){
+                  for(int j=0;j<M;j++){
+                    if (landpaths[first][i][j] != -1){
+                      int temp = 0;
+                      if (i>0){
+                        if (landpaths[first][i-1][j] != -1) temp+=landpaths[first][i-1][j];
+                      }
+                      if (i<N-1){
+                        if (landpaths[first][i+1][j] != -1) temp+=landpaths[first][i+1][j];
+                      }
+                      if (j>0){
+                        if (landpaths[first][i][j-1] != -1) temp+=landpaths[first][i][j-1];
+                      }
+                      if (j<M-1){
+                        if (landpaths[first][i][j+1] != -1) temp+=landpaths[first][i][j+1];
+                      }
+                      landpaths[second][i][j] = temp;
                     }
+                  }
                 }
             }
-            landpaths[R1][C1] = 1;
-            input.close();
-            for (int i = T;i>0;i--){
-                
-            }
-
-            System.out.println(Arrays.toString(coor2));
-            return landpaths[coor2[0]][coor2[1]];
+            System.out.println(Arrays.deepToString(landpaths));
+            return landpaths[second][R2][C2];
         }catch(FileNotFoundException e){
             return -1;
         }
     }
 
 
-    
+
 }
