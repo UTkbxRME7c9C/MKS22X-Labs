8e7c32f14aed9dd179d7a1361c4090599eec79cf
rafaye <sony89me86player@gmail.com>
2022-03-16 21:57:47 -0400

    finished quickselect

--- 05-quick/Quick.java
+++ 05-quick/Quick.java
@@ -2,11 +2,12 @@ import java.util.*;
 public class Quick{
     private static Random rand = new Random();
     public static void main(String[] args){
-        int [] data = new int[] {7,6,5,4,3,2,1,0};
+        //int [] data = new int[] {7,6,5,4,3,2,1,0};
+        int[] data = {17, 61, 67, 47, 93,12, 20, 4, 44, 68} ;
         System.out.println("Original: "+Arrays.toString(data));
-        int pivot = partition(data, 0, 4);
-        System.out.println("Pivot value: "+data[pivot]+ ", Pivot index: "+pivot);
-        System.out.println("Modified: "+Arrays.toString(data));
+        // int pivot = partition(data, 0, 4);
+         System.out.println("Pivot value: "+quickselect(data, 0)+ ", Pivot index: ");
+         System.out.println("Modified: "+Arrays.toString(data));
         System.out.println();
     }
     public static int partition(int[] data, int start, int end){
@@ -41,14 +42,18 @@ public class Quick{
         return start-1;
     }
     public static int quickselect(int[] data, int k){
-    /*return the value that is the kth smallest value of the array.
-    *@param data must have a length > 0
-    *@param k is 0 to data.length-1 inclusive
-    *@postcondition The array may be modified. */
-        int tempk = data.length-1;
-        int a =partition(data, 0, tempk);
-        
-        return 1;
+        int partstart = 0;
+        int partend = data.length-1;
+        while(true){
+            int p = partition(data, partstart, partend);
+            if (p == k){
+                 return data[k];
+            }else if (p < k){
+                partstart =p+1;
+            } else{
+                partend = p-1;
+            }
+        }
     }
     public static void quicksort(int[] data){
 
