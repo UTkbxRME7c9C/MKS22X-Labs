c07d172e810dc33b806b64b4cf446ab8bc569c78
baka <rhossain30@stuy.edu>
2022-03-21 13:51:10 -0500

    merge

---
+++ 06-merge/Merge.java
@@ -0,0 +1,19 @@
+public class Merge{
+    public static int [] merge(int [] left, int[] right){
+        
+        //return a new array that is the merged version of left and right
+    }
+
+    public static void mergesort(int [] data){
+        int [] temp  =  mergeSortH(data);
+        //copy temp into data
+    }
+
+    public static int[] mergesortH(int [] data){
+        // if more than 1 element{
+        //   left = copy half of data
+        //   right = copy other half of data
+        //   sort each half and merge them together
+        // }
+    }
+}
